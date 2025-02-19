96ca0aa96ea394cd98d0e863d1069256bb633142
hua <a1113791@gmail.com>
2025-02-19 16:42:13 +0800

    新增footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 // import { useState } from "react";
 // import axios from "axios";
+import Footer from "./layout/Footer";
 import Header from "./layout/Header";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
     <>
       <Header></Header>
+      <Footer></Footer>
     </>
   );
 }
