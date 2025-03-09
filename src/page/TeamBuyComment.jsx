998bcd3f2c3bf8d923749356359255975b0e236e
hua <a1113791@gmail.com>
2025-03-10 00:41:23 +0800

    新增揪團詳細頁

---
+++ src/page/TeamBuyComment.jsx
@@ -0,0 +1,79 @@
+function TeamBuyComment() {
+  return (
+    <>
+      <div className="container-fluid container-lg">
+        <div className="row d-flex justify-content-center">
+          <div className="col-xl-10">
+            <div className="mt-9 mb-6">
+              <h2 className="fs-h2 fw-bold">日期地點遊戲</h2>
+            </div>
+            <div className="border border-nature-90 rounded-xl">
+              <img src="" alt="遊戲圖" className="img-fluid" />
+              <div>
+                <table className="table">
+                  <tbody>
+                    <tr>
+                      <th scope="col">密室名稱</th>
+                      <th scope="col">揪團截止日期</th>
+                      <th scope="col">活動日期</th>
+                    </tr>
+                    <tr>
+                      <td>遊戲名</td>
+                      <td>日期</td>
+                      <td>日期</td>
+                    </tr>
+                    <tr>
+                      <th scope="col" colSpan="3">
+                        密室地址
+                      </th>
+                    </tr>
+                    <tr>
+                      <td colSpan="3">地址</td>
+                    </tr>
+                    <tr>
+                      <th scope="col">人數</th>
+                      <th scope="col">價格</th>
+                    </tr>
+                    <tr>
+                      <td>人數</td>
+                      <td>價格</td>
+                    </tr>
+                    <tr>
+                      <th scope="col">是否歡迎新手</th>
+                      <th scope="col">聯絡方式</th>
+                    </tr>
+                    <tr>
+                      <td>資訊</td>
+                      <td>資訊</td>
+                    </tr>
+                    <tr>
+                      <th scope="col" colSpan="3">
+                        揪團理念
+                      </th>
+                    </tr>
+                    <tr>
+                      <td colSpan="3">理念</td>
+                    </tr>
+                    <tr>
+                      <th scope="col" colSpan="3">
+                        報名者
+                      </th>
+                    </tr>
+                    <tr>
+                      <td colSpan="3">報名者</td>
+                    </tr>
+                  </tbody>
+                </table>
+                <div></div>
+                <div>
+                  <button></button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+export default TeamBuyComment;
