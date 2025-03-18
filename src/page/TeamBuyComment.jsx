b5382b1547843625429460f894f4d0c19eeb5f20
hua <a1113791@gmail.com>
2025-03-18 16:39:34 +0800

    修改揪團詳細RWD

--- src/page/TeamBuyComment.jsx
+++ src/page/TeamBuyComment.jsx
@@ -176,32 +176,32 @@ function TeamBuyComment() {
                 {`${group.game_name}`}
               </h2>
             </div>
-            <div className="border-nature-90 border-top border-right border-left">
-              <picture>
-                <source
-                  media="(min-width: 992px)"
-                  alt="banner"
-                  src={gameInfo?.game_img[0]}
-                  className="rounded-4"
-                />
-                <img
-                  src={gameInfo?.game_img[0]}
-                  alt="banner"
-                  className="w-100 img-fluid rounded-top ratio ratio-21x9"
-                  style={{
-                    objectFit: "cover",
-                  }}
-                />
-              </picture>
-            </div>
-            <div className="border border-nature-90 rounded-xl">
-              <div className="px-6 py-5 border-bottom border-nature-95">
+            <div>
+              <div className="border-nature-90 border-top border-right border-left">
+                <picture className="ratio ratio-16x9">
+                  <source
+                    media="(min-width: 992px)"
+                    alt="banner"
+                    src={gameInfo?.game_img[0]}
+                    className="rounded-4"
+                  />
+                  <img
+                    src={gameInfo?.game_img[0]}
+                    alt="banner"
+                    className="w-100 img-fluid rounded-top"
+                    style={{
+                      objectFit: "cover",
+                    }}
+                  />
+                </picture>
+              </div>
+              <div className="border border-nature-90 rounded-2 rounded-lg-4 px-4 px-lg-6">
                 <div className="d-flex mt-4 mb-5 mx-lg-6 mx-4">
                   <img
                     src={
-                      userInfo.user_sex === "男"
+                      userInfo?.user_sex === "男"
                         ? "./icon/man.png"
-                        : userInfo.user_sex === "女"
+                        : userInfo?.user_sex === "女"
                         ? "./icon/woman.png"
                         : "./icon/user.png"
                     }
@@ -217,184 +217,171 @@ function TeamBuyComment() {
                     {userInfo?.user_name}
                   </p>
                 </div>
-                <div>
-                  <table className="table table-borderless">
-                    <tbody className="row">
-                      <tr>
-                        <th scope="col" className="text-primary-50">
-                          密室名稱
-                        </th>
-                        <th scope="col" className="text-primary-50">
-                          揪團截止日期
-                        </th>
-                        <th scope="col" className="text-primary-50">
-                          活動日期
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td>{group.game_name}</td>
-                        <td>{group.group_end_at}</td>
-                        <td>{group.group_active_date}</td>
-                      </tr>
-                      <tr>
-                        <th scope="col" colSpan="3" className="text-primary-50">
-                          密室地址
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td colSpan="3">{group.game_address}</td>
-                      </tr>
-                      <tr>
-                        <th scope="col" className="text-primary-50">
-                          需求人數
-                        </th>
-                        <th scope="col" colSpan="2" className="text-primary-50">
-                          價格
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td>{group.group_member}人</td>
-                        <td colSpan="2">
-                          <ul className="d-flex flex-column gap-2">
-                            {priceInfo.length === 2 ? (
-                              // 當有兩筆價格資料，顯示平日與假日
-                              <li className="fs-Body-2">
-                                {(() => {
-                                  const weekdayPrice = priceInfo.find(
-                                    (item) => item.price_day_type === "weekday"
-                                  );
-                                  const weekendPrice = priceInfo.find(
-                                    (item) => item.price_day_type === "weekend"
-                                  );
-                                  return (
-                                    <>
-                                      {weekdayPrice && (
-                                        <span>
-                                          平日 {weekdayPrice.price_people}：$
-                                          {weekdayPrice.price_mix}
-                                        </span>
-                                      )}
-                                      <br />
-                                      {weekendPrice && (
-                                        <span>
-                                          假日 {weekendPrice.price_people}：$
-                                          {weekendPrice.price_mix}
-                                        </span>
-                                      )}
-                                    </>
-                                  );
-                                })()}
-                              </li>
-                            ) : (
-                              // 當只有一筆資料時，直接顯示價格，不顯示 price_day_type 文字
-                              priceInfo.map((item) => (
-                                <li key={item.price_id} className="fs-Body-2">
-                                  {item.price_people}：${item.price_mix}
-                                </li>
-                              ))
-                            )}
-                          </ul>
-                        </td>
-                      </tr>
-                      <tr>
-                        <th scope="col" className="text-primary-50">
-                          是否歡迎新手
-                        </th>
-                        <th scope="col" colSpan="2" className="text-primary-50">
-                          聯絡方式
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td>{group.group_noob ? "是" : "否"}</td>
-                        <td colSpan="2">{group.group_channel}</td>
-                      </tr>
-                      <tr>
-                        <th scope="col" colSpan="3" className="text-primary-50">
-                          揪團理念
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td colSpan="3">{group.group_philosophy}</td>
-                      </tr>
-                      <tr>
-                        <th scope="col" colSpan="3" className="text-primary-50">
-                          報名者
-                        </th>
-                      </tr>
-                      <tr className="border-bottom border-nature-95">
-                        <td colSpan="3">
-                          {group.group_participants &&
-                          group.group_participants.length > 0
-                            ? group.group_participants.map((userId, index) => {
-                                const participant = users.find(
-                                  (u) => u.user_id === userId
-                                );
-                                return (
-                                  <span key={userId}>
-                                    {participant
-                                      ? participant.user_name
-                                      : userId}
-                                    {index !==
-                                      group.group_participants.length - 1 &&
-                                      ", "}
-                                  </span>
-                                );
-                              })
-                            : "尚無參與者"}
-                        </td>
-                      </tr>
-                      <tr>
-                        <td colSpan="3">
-                          <div className="tags d-flex flex-wrap fs-Body-2 gap-2 mt-3 ">
-                            <span className=" bg-nature-95 px-1 py-1 rounded-3  text-nowrap">
-                              {gameInfo?.game_dif_tagname}
-                            </span>
-                            <span className=" bg-nature-95 px-1 py-1 rounded-3 text-nowrap">
-                              {gameInfo?.game_main_tag1name}
-                            </span>
-                            <span className=" bg-nature-95 px-1 py-1 rounded-3  text-nowrap">
-                              {gameInfo?.game_main_tag2name}
-                            </span>
-                          </div>
-                        </td>
-                      </tr>
-                      <tr>
-                        <td className="my-5" colSpan="3">
-                          {!user || !user_token ? (
-                            <span
-                              className="d-inline-block"
-                              tabIndex="0"
-                              data-bs-toggle="tooltip"
-                              data-bs-placement="right"
-                              title="請先登入"
-                            >
-                              <button
-                                type="button"
-                                className="btn btn-secondary-60 text-white px-17 py-2"
-                                onClick={changeGroup}
-                                disabled={!user || !user_token}
-                              >
-                                我要參加
-                              </button>
-                            </span>
-                          ) : (
-                            <button
-                              type="button"
-                              className="btn btn-secondary-60 text-white px-17 py-2"
-                              onClick={changeGroup}
-                              disabled={!user || !user_token}
-                            >
-                              我要參加
-                            </button>
-                          )}
-                        </td>
-                      </tr>
-                    </tbody>
-                  </table>
+                <div className="container py-5">
+                  <div className="row gy-4">
+                    <div className="col-12 col-lg-4">
+                      <p className="text-primary-50 fs-Body-2 mb-2">密室名稱</p>
+                      <p>{group.game_name}</p>
+                    </div>
+                    <div className="col-12 col-lg-4">
+                      <p className="text-primary-50 fs-Body-2 mb-2">
+                        揪團截止日期
+                      </p>
+                      <p>{group.group_end_at}</p>
+                    </div>
+                    <div className="col-12 col-lg-4">
+                      <p className="text-primary-50 fs-Body-2 mb-2">活動日期</p>
+                      <p>{group.group_active_date}</p>
+                    </div>
+                  </div>
+                  <div className="row gy-4">
+                    <div className="col-12">
+                      <p className="text-primary-50 fs-Body-2 mb-2">密室地址</p>
+                      <p>{group.game_address}</p>
+                    </div>
+                  </div>
+                  <div className="row gy-4">
+                    <div className="col-6 col-lg-2">
+                      <p className="text-primary-50 fs-Body-2 mb-2">需求人數</p>
+                      <p>{group.group_member}人</p>
+                    </div>
+                    <div className="col-6 col-lg-10">
+                      <p className="text-primary-50 fs-Body-2 mb-2">價格</p>
+                      <ul>
+                        {priceInfo.length === 2 ? (
+                          // 當有兩筆價格資料，顯示平日與假日
+                          <li className="fs-Body-2">
+                            {(() => {
+                              const weekdayPrice = priceInfo.find(
+                                (item) => item.price_day_type === "weekday"
+                              );
+                              const weekendPrice = priceInfo.find(
+                                (item) => item.price_day_type === "weekend"
+                              );
+                              return (
+                                <>
+                                  {weekdayPrice && (
+                                    <span>
+                                      平日 {weekdayPrice.price_people}：$
+                                      {weekdayPrice.price_mix}
+                                    </span>
+                                  )}
+                                  <br />
+                                  {weekendPrice && (
+                                    <span>
+                                      假日 {weekendPrice.price_people}：$
+                                      {weekendPrice.price_mix}
+                                    </span>
+                                  )}
+                                </>
+                              );
+                            })()}
+                          </li>
+                        ) : (
+                          // 當只有一筆資料時，直接顯示價格，不顯示 price_day_type 文字
+                          priceInfo.map((item) => (
+                            <li key={item.price_id} className="fs-Body-2">
+                              {item.price_people}：${item.price_mix}
+                            </li>
+                          ))
+                        )}
+                      </ul>
+                    </div>
+                  </div>
+                  <div className="row gy-4">
+                    <div className="col-6 col-lg-2">
+                      <p className="text-primary-50 fs-Body-2 mb-2">
+                        是否歡迎新手
+                      </p>
+                      <p>{group.group_noob ? "是" : "否"}</p>
+                    </div>
+                    <div className="col-6 col-lg-10">
+                      <p className="text-primary-50 fs-Body-2 mb-2">聯絡方式</p>
+                      <p>{group.group_channel}</p>
+                    </div>
+                  </div>
+                  <div className="row gy-4">
+                    <div className="col-12">
+                      <p className="text-primary-50 fs-Body-2 mb-2">揪團理念</p>
+                      <p>{group.group_philosophy}</p>
+                    </div>
+                  </div>
+                  <div className="row gy-4">
+                    <div className="col-12">
+                      <p className="text-primary-50 fs-Body-2 mb-2">報名者</p>
+                      <p>
+                        {group.group_participants &&
+                        group.group_participants.length > 0
+                          ? group.group_participants.map((userId, index) => {
+                              const participant = users.find(
+                                (u) => u.user_id === userId
+                              );
+                              return (
+                                <span key={userId}>
+                                  {participant ? participant.user_name : userId}
+                                  {index !==
+                                    group.group_participants.length - 1 && ", "}
+                                </span>
+                              );
+                            })
+                          : "尚無參與者"}
+                      </p>
+                      <p className="text-primary-50 fs-Body-2">
+                        共{group.group_participants.length}人加入此揪團
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="pb-4">
+                  <div className="tags d-flex flex-wrap fs-Body-2 gap-2 mt-3 ">
+                    <span className=" bg-nature-95 px-1 py-1 rounded-3  text-nowrap">
+                      {gameInfo?.game_dif_tagname}
+                    </span>
+                    <span className=" bg-nature-95 px-1 py-1 rounded-3 text-nowrap">
+                      {gameInfo?.game_main_tag1name}
+                    </span>
+                    <span className=" bg-nature-95 px-1 py-1 rounded-3  text-nowrap">
+                      {gameInfo?.game_main_tag2name}
+                    </span>
+                  </div>
+                </div>
+                <div className="m-4 my-lg-5">
+                  {!user || !user_token ? (
+                    <span
+                      className="d-inline-block"
+                      tabIndex="0"
+                      data-bs-toggle="tooltip"
+                      data-bs-placement="right"
+                      title="請先登入"
+                    >
+                      <div className="d-grid gap-2 col-12 mx-auto d-lg-block">
+                        <button
+                          type="button"
+                          className="btn btn-secondary-60 text-white"
+                          onClick={changeGroup}
+                          disabled={!user || !user_token}
+                        >
+                          我要參加
+                        </button>
+                      </div>
+                    </span>
+                  ) : (
+                    <div className="d-grid gap-2 col-12 mx-auto d-lg-block">
+                      <button
+                        type="button"
+                        className="btn btn-secondary-60 text-white"
+                        onClick={changeGroup}
+                        disabled={!user || !user_token}
+                      >
+                        我要參加
+                      </button>
+                    </div>
+                  )}
                 </div>
               </div>
             </div>
-            <div className="row m-0 mt-3">
+
+            <div className="row m-0 mt-7 mt-lg-6">
               <div className="title-container w-100 d-flex justify-content-center align-items-center">
                 <h3 className="text-center mb-12 recommendation-title fw-bold fs-sm-h3 fs-h6">
                   相關推薦
